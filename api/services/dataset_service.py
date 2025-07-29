b3951e62a11bd6518ee0186fe4bf7fa2d5b09704
sakura-sunby <49379871+sakura-sunby@users.noreply.github.com>
2025-07-29 11:05:15 +0800

    auto_upgrade

--- api/services/dataset_service.py
+++ api/services/dataset_service.py
@@ -651,7 +651,7 @@ class DocumentService:
             document_ids = []
             document_ids.append(document.id)
             # 触发异步任务进行文档索引更新
-            document_indexing_task.delay(document.dataset_id, document_ids, None)
+            document_indexing_task.delay(document.dataset_id, document_ids, document.split_strategy_dict)
 
         except Exception as e:
             logging.exception(f"Failed to update document {document.id} with file {file.id}")
